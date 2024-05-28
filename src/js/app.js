e61843e0f09838b099195bfae2909d58b46028c9
JoachimAndreBergan <jobe016@student.kristiania.no>
2024-05-28 16:32:41 +0200

    added js function

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,21 @@
 
 
 //selecting the elements from the dom
+const selectElement = document.querySelector('.format');
+const isbn = document.querySelector('.isbn');
+const narrator = document.querySelector('.narrator');
 
+
+
+
+
+// Adding all the event listeners
+selectElement.addEventListener('change', ()=>{
+    if(selectElement.value === 'physical'){
+        narrator.setAttribute('disabled', '');
+        isbn.removeAttribute('disabled');
+    } else{
+        isbn.setAttribute('disabled', '');
+        narrator.removeAttribute('disabled'); 
+    }
+})
